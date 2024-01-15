42531a93d9384a1789286f5187547dd220169b3d
Saurabh Jadli <86057535+SaurabhJadli@users.noreply.github.com>
2024-01-15 21:34:43 +0530

    func

---
+++ function.py
@@ -0,0 +1,11 @@
+def printline():    #function defination (formal)
+    print("\nthis line is within the function.")        #function body
+    print("another line inside the fun\n")
+    return 0
+
+#printline()         #function calling (actual)
+
+#function has 3 main components:
+# 1. function defination
+# 2. function body
+# 3. fuction calling
