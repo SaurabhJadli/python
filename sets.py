46ae6b36cdb4369909f150a4d3d76afd0953b420
Saurabh Jadli <86057535+SaurabhJadli@users.noreply.github.com>
2024-01-29 23:02:19 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,13 @@
+print("Sets in python:")
+s = {1,2,3,4,5,5}
+print(s)
+print("\n sets can't have duplicate values and is unordered.")
+
+s1 = {1,2,4}
+s2 = {1,3,5}
+print(s1.intersection(s2))
+
+print("\nEmpty set:")
+s3 = {}
+print(s3)
+print(type(s3))
